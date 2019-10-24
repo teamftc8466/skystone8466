02031721acf82eb9a05a34b8289e06d2f770f1ec
Lucas feldsien <lbfeldsien@gmail.com>
2019-10-23 21:04:09 -0700

    first first code for the lifter
    this is the rought draft test code for the lifter and inplementing the s-curve

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -1,44 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Lift {
 
-    private Telemetry telemetry_ = null;
-
-    ///Motors
+    //Motors
     DcMotor motorLift_;
 
-    ///Servos
-    Servo servo1_;
-    Servo servo2_; //TODO: Rename later when you know the functions
-
-    ///Constructor
-    public Lift (DcMotor motor_lift,
-                 Servo servo_1,
-                 Servo servo_2,
-                 Telemetry telemetry) {
+    //Constructor
+    public Lift (DcMotor motor_lift) {
         motorLift_ = motor_lift;
-        servo1_ = servo_1;
-        servo2_ = servo_2;
-        telemetry_ = telemetry;
 
-        motorLift_.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        ///Reverse if necessary
-        //motorLift_.setDirection(DcMotor.Direction.REVERSE);
-
-        useEncoder();
     }
 
-    void useEncoder(){
-        motorLift_.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+    public void loop(Gamepad g){
+
     }
 
-    void resetEncoder() {
-        motorLift_.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    public void auto(){
+
     }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP/pulleytest.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.teleOP;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import java.util.concurrent.TimeUnit;
+
+//@Disabled
+@TeleOp(name = "WinchTest1")
+public class pulleytest extends OpMode {
+
+    //motor declare
+    public DcMotor pulleymotor1;
+    //public double functime = getRuntime();
+    public double deltafunctime = 0;
+    public double motorpower1 = 0;//same with motorpower
+
+        public void init() {
+            //obtains the hardware map
+            HardwareMap hvm = hardwareMap;
+            pulleymotor1 = hvm.get(DcMotor.class, "pulleymotor1");
+        }
+
+        public void loop() {
+            //I maka da deadzone
+            gamepad1.setJoystickDeadzone(0.1f);
+            if(gamepad1.right_stick_y < .1 && gamepad1.right_stick_y > -.1) {
+                pulleymotor1.setPower(0);
+                deltafunctime = 0;
+                //functime = getRuntime();
+            }
+            else {
+                do {
+                    motorpower1 = Math.pow((1/300) * deltafunctime * (1/gamepad1.right_stick_y), 3);
+                    pulleymotor1.setPower(motorpower1);
+                    //pulleymotor1.setPower(motorpower1);
+
+                    deltafunctime++;
+                }
+                while(deltafunctime<=300);
+
+                if(deltafunctime>300) {
+                    pulleymotor1.setPower(gamepad1.right_stick_y);
+                }
+
+
+                /*while (deltafunctime<20) {
+                    motorpower1 = Math.pow((1 / 20) * deltafunctime * (1 / gamepad1.right_stick_y), 3);
+                    pulleymotor1.setPower(motorpower1);
+
+                    deltafunctime++;
+
+                    try {
+                        Thread.sleep(100);
+                    }
+                    catch (InterruptedException e) {
+
+                            e.printStackTrace();
+                        }
+                }
+
+                if (deltafunctime>=20) {
+                    pulleymotor1.setPower(gamepad1.right_stick_y);
+                }*/
+
+
+
+
+                /*if (deltafunctime > 0 && deltafunctime <= 5) {
+
+                    deltafunctime = getRuntime() - functime;
+                    motorpower1 = Math.pow((1 / 3) * deltafunctime * (1 / gamepad1.right_stick_y), 3);
+
+                    pulleymotor1.setPower(motorpower1);
+
+                }
+                else if (deltafunctime > 5) {
+                    pulleymotor1.setPower(gamepad1.right_stick_y);
+                }*/
+            }
+        }
+    }
+
+//motor wheel diameter 21.95mm

0ad78f6c3c3e898a76996b97508eea5003a5ce95
Lucas feldsien <lbfeldsien@gmail.com>
2019-11-03 15:18:15 -0800

    update to lifter test code
    no progress achieved but these are the changes that occured

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP/pulleytest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP/pulleytest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -15,37 +16,56 @@ public class pulleytest extends OpMode {
 
     //motor declare
     public DcMotor pulleymotor1;
-    //public double functime = getRuntime();
-    ElapsedTime functtime = new ElapsedTime();
+    public double functime = getRuntime();
+    //ElapsedTime functtime = new ElapsedTime();
     //public double deltafunctime = 0;
     public double motorpower1 = 0;//same with motorpower
 
         public void init() {
             //obtains the hardware map
+            pulleymotor1.setDirection(DcMotor.Direction.FORWARD);
             HardwareMap hvm = hardwareMap;
             pulleymotor1 = hvm.get(DcMotor.class, "pulleymotor1");
         }
 
         public void loop() {
             //I maka da deadzone
-            if(gamepad1.right_stick_y < .1 && gamepad1.right_stick_y > -.1) {
+            /*if(gamepad1.right_stick_y < .1 && gamepad1.right_stick_y > -.1) {
                 pulleymotor1.setPower(0);
                 functtime.reset();
                 //functime = getRuntime();
             }
-            else {
 
-                if(functtime.time() <= 10) {
-                    motorpower1 = Math.pow((1 / 10) * functtime.time() * (1 / gamepad1.right_stick_y), 3);
+            else {
+                if(functtime.time() <= 10000) {
+                    motorpower1 = Math.pow((1/10000)*functtime.time()*(1 / gamepad1.right_stick_y), 3);
                     pulleymotor1.setPower(motorpower1);
                     //pulleymotor1.setPower(motorpower1);
-
-
                 }
-                if(functtime.time() > 10) {
+
+                if(functtime.time() > 10000) {
                     pulleymotor1.setPower(gamepad1.right_stick_y);
-                }
+                }*/
 
+
+                if(gamepad1.right_stick_y < .1 && gamepad1.right_stick_y > -.1) {
+                pulleymotor1.setPower(0);
+                this.resetStartTime();
+                System.out.println(this.getRuntime());
+            }
+            else {
+
+                if(this.getRuntime() <= 10000) {
+                    //deltafunctime = this.getRuntime()-functime;
+                    motorpower1 = Math.pow((1/10000)*this.getRuntime()*(1/gamepad1.right_stick_y), 3);
+                    pulleymotor1.setPower(motorpower1);
+                    //pulleymotor1.setPower(motorpower1);
+                    System.out.println(this.getRuntime());
+                }
+                if(this.getRuntime() > 10000) {
+                    pulleymotor1.setPower(gamepad1.right_stick_y);
+                    System.out.println(this.getRuntime());
+                }
 
                 /*while (deltafunctime<20) {
                     motorpower1 = Math.pow((1 / 20) * deltafunctime * (1 / gamepad1.right_stick_y), 3);

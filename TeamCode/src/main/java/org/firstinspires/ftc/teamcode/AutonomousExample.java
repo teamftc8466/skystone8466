ae890de3fb3c742d440ec4df1b1d10ceaf3a3cd1
teamftc8466 <melindalin314@gmail.com>
2019-12-04 20:47:31 -0800

    Created an simple autonomous program that drives towards the skystone and parks under the bridge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousExample.java
@@ -9,10 +9,12 @@ public class AutonomousExample extends AutonomousCommon {
 
     AutoOperation [] opExample_ = {
             new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_RESET_ENCODER, 0.1),
-            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_FORWARD, 1.0),
-            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_BACKWARD, 1.0),
-            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_SHIFT_LEFT, 1.0),
-            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_SHIFT_RIGHT, 1.0),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_SHIFT_LEFT, 0.15),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_FORWARD, 1.05),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_BACKWARD, 0.15),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_TURN_RIGHT, 90),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_FORWARD, 2.95),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_BACKWARD, 1.03),
             new AutoOperation(AutoOperation.OpCode.OP_STOP, 0.1)
     };
 

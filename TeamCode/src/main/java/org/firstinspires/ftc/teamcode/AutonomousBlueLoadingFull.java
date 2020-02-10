4a64ae04a54e549defec95f0b27de858b9d5c9a1
Melinda Lin <melindalin314@gmail.com>
2020-02-09 22:32:45 -0800

    Tune up autonomous at blue loading position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueLoadingFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueLoadingFull.java
@@ -36,7 +36,7 @@ public class AutonomousBlueLoadingFull extends AutonomousCommon {
             new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_BACKWARD, 0.45),
             new AutoOperation(AutoOperation.OpCode.OP_LIFT_MOVE_TO_BOTTOM_POSITION, 0),
             new AutoOperation(AutoOperation.OpCode.OP_GRABBER_CRANE_FULL_DRAW_BACK, 0),
-            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_SHIFT_RIGHT, 0.20),
+            new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_SHIFT_RIGHT, 0.10),
             new AutoOperation(AutoOperation.OpCode.OP_DRIVE_TRAIN_BACKWARD, 0.6),
             // new AutoOperation(AutoOperation.OpCode.OP_GRABBER_CRANE_FULL_DRAW_BACK, 0),
             // new AutoOperation(AutoOperation.OpCode.OP_WAIT, 3),
